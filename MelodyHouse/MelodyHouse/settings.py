8619cb8ddbd99131b9e5b19efb8e3cac82ab57cc
Koushik-Sarker-Seemanto <koushik24@student.sust.edu>
2019-09-22 07:04:47 +0600

    little update

--- MelodyHouse/MelodyHouse/settings.py
+++ MelodyHouse/MelodyHouse/settings.py
@@ -90,7 +90,7 @@ AUTH_USER_MODEL = 'home.Account'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'seemanto1997@gmail.com'
-EMAIL_HOST_PASSWORD = 'Hello2441139'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
