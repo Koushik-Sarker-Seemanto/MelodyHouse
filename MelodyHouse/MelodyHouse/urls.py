81b06bab37aae6a4151f58bc8785d0ac135a62be
Koushik-Sarker-Seemanto <koushik24@student.sust.edu>
2019-10-16 15:47:19 +0600

    audio file handled

--- MelodyHouse/MelodyHouse/urls.py
+++ MelodyHouse/MelodyHouse/urls.py
@@ -25,3 +25,7 @@ urlpatterns = [
     url(r'', include('profile_app.urls')),
     url(r'', include('upload_app.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- MelodyHouse/upload_app/views.py
+++ MelodyHouse/upload_app/views.py
@@ -7,6 +7,9 @@ from authentication.models import Account
 from upload_app.models import Album, Song
 from upload_app.forms import AlbumForm, SongForm
 
+AUDIO_FILE_TYPES = ['wav', 'mp3', 'ogg']
+IMAGE_FILE_TYPES = ['png', 'jpg', 'jpeg']
+
 
 @login_required(login_url='/signin/')
 def addAlbum(request):
@@ -20,7 +23,14 @@ def addAlbum(request):
             album_title = form.cleaned_data['album_title']
             query = Album.objects.filter(artist=artist).filter(album_title=album_title).filter(user=request.user)
             if query.exists():
-                return HttpResponse('<h1 align="center" style="color: red;margin-top: 400px">Album Already Exists!!!</h1>')
+                form = AlbumForm()
+                form_song = SongForm()
+                context = {
+                    'form': form,
+                    'form_song': form_song,
+                    'error_message': 'Album Already Exists!!!',
+                }
+                return render(request, 'upload_app/uploadPage.html', context)
             else:
                 album.save()
                 return redirect('profile_app:profile-view')
@@ -29,13 +39,32 @@ def addAlbum(request):
         if form_song.is_valid():
             song = form_song.save(commit=False)
 
+            song.song_file = request.FILES['song_file']
+            file_type = song.song_file.url.split('.')[-1]
+            file_type = file_type.lower()
+            if file_type not in AUDIO_FILE_TYPES:
+                form = AlbumForm()
+                form_song = SongForm()
+                context = {
+                    'form': form,
+                    'form_song': form_song,
+                    'error_message': 'Audio file must be WAV, MP3, or OGG',
+                }
+                return render(request, 'upload_app/uploadPage.html', context)
+
             song_file = form_song.cleaned_data['song_file']
             album_id = form_song.cleaned_data['album_id']
 
             query = Song.objects.filter(album_id=album_id).filter(song_file=song_file)
             if query.exists():
-                return HttpResponse(
-                    '<h1 align="center" style="color: red;margin-top: 400px">Song Already Exists!!!</h1>')
+                form = AlbumForm()
+                form_song = SongForm()
+                context = {
+                    'form': form,
+                    'form_song': form_song,
+                    'error_message': 'Song Already Exists!!!',
+                }
+                return render(request, 'upload_app/uploadPage.html', context)
             else:
                 song.save()
                 return redirect('profile_app:profile-view')
