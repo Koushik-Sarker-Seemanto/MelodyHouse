c5d011e489984519880f3ee8cde09ad4e6b388f4
Koushik-Sarker-Seemanto <koushik24@student.sust.edu>
2019-10-16 14:59:11 +0600

    song added

--- MelodyHouse/MelodyHouse/settings.py
+++ MelodyHouse/MelodyHouse/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap_modal_forms',
 
     'home.apps.HomeConfig',
     'authentication.apps.AuthenticationConfig',
--- MelodyHouse/upload_app/models.py
+++ MelodyHouse/upload_app/models.py
@@ -17,13 +17,28 @@ _GENRES = (
 
 class Album(models.Model):
     user = models.ForeignKey(Account, on_delete=models.CASCADE)
-    artist = models.CharField(max_length=250)
-    album_title = models.CharField(max_length=500)
+    artist = models.CharField(max_length=100)
+    album_title = models.CharField(max_length=100)
     genre = models.CharField(max_length=50, choices=_GENRES)
-    description = models.CharField(max_length=1000, default='', blank=True)
+    description = models.TextField(max_length=1000, default='', blank=True)
 
     class Meta:
         unique_together = (('album_title', 'artist', 'user'),)
 
     def __str__(self):
         return self.album_title + ' - ' + self.artist
+
+
+class Song(models.Model):
+    song_title = models.CharField(max_length=100)
+    song_file = models.CharField(max_length=500)
+    album_id = models.ForeignKey(Album, on_delete=models.CASCADE, )
+    description = models.TextField(max_length=1000, default='', blank=True)
+
+    class Meta:
+        unique_together = (('song_file', 'album_id'), )
+
+    def __str__(self):
+        return self.song_title
+
+               # + Album.objects.filter(album_id=self.album_id)
